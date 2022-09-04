b1d4cdb12e25bdd71e79d05dffc53353050b087b
DuTran7 <duu.trancong@gmail.com>
2022-09-04 12:36:23 +0700

    Chore: remove update config nextjs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins');
-const withImages = require('next-images');
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['media.urbanistnetwork.com'],
-  },
 };
 
-module.exports = withPlugins([[withImages]], nextConfig);
+module.exports = nextConfig;
