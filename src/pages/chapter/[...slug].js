a27db681cf183ae1ad7287d467fc4a492e31c2bb
DuTran7 <duu.trancong@gmail.com>
2022-11-25 20:26:26 +0700

    Add seo chapter detail

--- src/pages/chapter/[...slug].js
+++ src/pages/chapter/[...slug].js
@@ -1,27 +1,55 @@
 import ChapterDetail from 'components/pages/Chapter/ChapterDetail';
 import { getChapterDetail } from 'components/service/CategoryDetailService';
+import { NextSeo } from 'next-seo';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { IMAGE_SOURCE } from 'utils/constants';
 
-export default function Chapter() {
-  const router = useRouter();
-  const [data, setData] = useState(null);
-  const updateData = async (id) => {
-    const res = await getChapterDetail(id);
-    if (res?.status === 200) {
-      setData(res?.data);
-    } else {
-      alert('Chapter was wrong');
-      router.push('/');
-    }
-  };
-  useEffect(() => {
-    const id = router.query.slug?.[0];
-    if (id) updateData(id);
-  }, [router.query.slug]);
+export default function Chapter({ chapterInfo, seo }) {
   return (
     <>
-      <ChapterDetail data={data} />
+      <NextSeo {...seo} />
+      <ChapterDetail data={chapterInfo} />
     </>
   );
+}
+export async function getStaticProps({ params }) {
+  const id = params?.slug?.[0];
+  // Call an external API endpoint to get posts
+  const res = await getChapterDetail(id);
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  const detailCategory = res?.data?.detailCategory;
+  const seoConfig = {
+    title: detailCategory?.name,
+    description:
+      detailCategory?.description?.split('<cap>')?.[0] +
+        ' ' +
+        detailCategory?.description?.split('<cap>')?.[1] || '',
+    openGraph: {
+      type: 'website',
+      title: 'Albums',
+      images: res?.data?.albums?.map((img) => ({
+        url: IMAGE_SOURCE + img?.name,
+        width: 800,
+        height: 600,
+        alt: img?.description,
+      })),
+    },
+  };
+  return {
+    props: {
+      // posts,
+      chapterInfo: res?.data,
+      seo: seoConfig,
+    },
+  };
+}
+export async function getStaticPaths(b) {
+  return {
+    paths: [{ params: { slug: ['1', 'some thing'] } }],
+    fallback: 'blocking', // can also be true or 'blocking'
+  };
 }
